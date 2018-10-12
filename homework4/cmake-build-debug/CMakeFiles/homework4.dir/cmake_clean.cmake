20ecec1e2b02a39398f68066ca196fbfac68bd5d
Aaron723 <w745207699@gmail.com>
2018-10-12 09:48:19 -0400

    refine a bug

---
+++ homework4/cmake-build-debug/CMakeFiles/homework4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework4.dir/main.cpp.o"
+  "homework4.pdb"
+  "homework4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
